a3b56e85f8f02d06a4f0bdf945af41401355aa67
zhemchuzhnyi <a_reshkin@inbox.ru>
2025-02-24 00:42:23 +0300

    Lesson_1 - "first acquaintance with java"

---
+++ src/main/practice/Lesson_1.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Lesson_1 {
+    public static void main(String[] args) {
+        String i = "Привет. ";
+        String o = "Меня зовут Андрей! \n";
+        String p = "Рад познакомиться!) ";
+        String a = "Я начинаю изучение языка Java!";
+        System.out.println(i + o + p + a);
+    }
+}
